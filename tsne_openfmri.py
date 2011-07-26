90f6f943e436b5451958e91d485a308f4cbba4a4
Russ Poldrack <poldrack@gmail.com>
2011-07-26 18:49:51 -0500

    added license

--- tsne_openfmri.py
+++ tsne_openfmri.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python
+""" tsne_openfmri.py: code to run t-SNE on results from MELODIC ICA applied to the OpenFMRI dataset
+
+requires:
+- tsne.py from http://homepage.tudelft.nl/19j49/t-SNE.html
+- Numpy
+"""
+
+## Copyright 2011, Russell Poldrack. All rights reserved.
+
+## Redistribution and use in source and binary forms, with or without modification, are
+## permitted provided that the following conditions are met:
+
+##    1. Redistributions of source code must retain the above copyright notice, this list of
+##       conditions and the following disclaimer.
+
+##    2. Redistributions in binary form must reproduce the above copyright notice, this list
+##       of conditions and the following disclaimer in the documentation and/or other materials
+##       provided with the distribution.
+
+## THIS SOFTWARE IS PROVIDED BY RUSSELL POLDRACK ``AS IS'' AND ANY EXPRESS OR IMPLIED
+## WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+## FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL RUSSELL POLDRACK OR
+## CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+## CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+## SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+## ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+## NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+## ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
 
 from tsne import *
 import numpy as N
