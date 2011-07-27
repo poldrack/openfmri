246a9a7a4fd96d9c40a0d86de758860a4c9ca878
Russell Poldrack <poldrack@login4.ls4.tacc.utexas.edu>
2011-07-27 18:17:25 -0500

    improved run_betfunc

--- run_betfunc.py
+++ run_betfunc.py
@@ -1,4 +1,7 @@
 """ run_betfunc.py - run bet on functional images
+
+USAGE: python run_betfnc.py <name of dataset> <basedir - default is staged>
+
 """
 
 ## Copyright 2011, Russell Poldrack. All rights reserved.
@@ -25,17 +28,38 @@
 
 
 import os
+import sys
 
-basedir='/corral/utexas/poldracklab/openfmri/shared/'
-outfile=open('run_betfunc.sh','w')
+def main():
 
-for root,dirs,files in os.walk(basedir):
-    for f in files:
-        if f.rfind('bold_mcf.nii.gz')>-1:
-            outfile.write('bet %s/%s %s/%s -F\n'%(root,f,root,f.replace('mcf','mcf_brain')))
+    if len(sys.argv)>1:
+        ds=sys.argv[1]
+    else:
+        usage()
 
-outfile.close()
+    if len(sys.argv)>2:
+        basedir=sys.argv[2]
+        if not os.path.exists(basedir):
+            print 'basedir %s does not exist!'%basedir
+            sys.exit(1)
+    else:
+        basedir='/corral/utexas/poldracklab/openfmri/staged/'
+        
+    outfile=open('run_mcflirt.sh','w')
 
-            
-print 'now launch using:'
-print 'launch -s run_betfunc.sh -n betfunc -r 00:30:00'
+
+    outfile=open('run_betfunc.sh','w')
+
+    for root,dirs,files in os.walk(basedir+ds):
+        for f in files:
+            if f.rfind('bold_mcf.nii.gz')>-1  and root.find(ds)>-1:
+                outfile.write('bet %s/%s %s/%s -F\n'%(root,f,root,f.replace('mcf','mcf_brain')))
+
+    outfile.close()
+
+
+    print 'now launch using:'
+    print 'launch -s run_betfunc.sh -n betfunc -r 00:10:00'
+
+if __name__ == '__main__':
+    main()
