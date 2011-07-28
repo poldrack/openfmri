cfef05eaa21d681d4b4f89b9312c1cd9f76c0035
Russell Poldrack <poldrack@login4.ls4.tacc.utexas.edu>
2011-07-27 21:52:52 -0500

    fixed level2 code

--- mk_all_level2_fsf.py
+++ mk_all_level2_fsf.py
@@ -1,4 +1,6 @@
 """ mk_all_level2_fsf.py - make level 2 fsf files for all subjects in a dataset
+USAGE: python mk_all_level2_fsf.py <name of dataset>  <basedir - default is staged>
+
 """
 
 ## Copyright 2011, Russell Poldrack. All rights reserved.
@@ -28,29 +30,54 @@
 import os
 from mk_level2_fsf import *
 
-basedir='/corral/utexas/poldracklab/openfmri/staged/'
-outfile=open('mk_all_level2_fsf.sh','w')
+import sys
 
-featdirs=[]
-subdirs={}
+def usage():
+    """Print the docstring and exit with error."""
+    sys.stdout.write(__doc__)
+    sys.exit(2)
 
-taskid_list=['ds001']
+def main():
 
-for taskid in taskid_list:
-  for root,dirs,files in os.walk(basedir):
-#    for f in files:
-        if root.split('/')[-1].rfind('.feat')>-1 and root.find(taskid)>-1:
-            featdirs.append(root)
-            fs=featdirs[-1].split('/')
-            subnum=int(fs[7].replace('sub',''))
-            if not subdirs.has_key(subnum):
-                subdirs[subnum]={}
-            runnum=int(fs[9].split('_')[1].split('.')[0].replace('run',''))
-            tasknum=int(fs[9].split('_')[0].replace('task',''))
-            if not subdirs[subnum].has_key(tasknum):
-                subdirs[subnum][tasknum]=[]
-            subdirs[subnum][tasknum].append(runnum)
+    if len(sys.argv)>1:
+        taskid=sys.argv[1]
+    else:
+        usage()
 
-  for s in subdirs.iterkeys():
-    for t in subdirs[s].iterkeys():
-        mk_level2_fsf(taskid,s,t,subdirs[s][t],basedir)
+
+    if len(sys.argv)>2:
+        basedir=sys.argv[2]
+        if not os.path.exists(basedir):
+            print 'basedir %s does not exist!'%basedir
+            sys.exit(1)
+    else:
+        basedir='/corral/utexas/poldracklab/openfmri/staged/'
+
+
+
+    outfile=open('mk_all_level2_fsf.sh','w')
+
+    featdirs=[]
+    subdirs={}
+
+
+    for root,dirs,files in os.walk(basedir):
+    #    for f in files:
+            if root.split('/')[-1].rfind('.feat')>-1 and root.find(taskid)>-1:
+                featdirs.append(root)
+                fs=featdirs[-1].split('/')
+                subnum=int(fs[7].replace('sub',''))
+                if not subdirs.has_key(subnum):
+                    subdirs[subnum]={}
+                runnum=int(fs[9].split('_')[1].split('.')[0].replace('run',''))
+                tasknum=int(fs[9].split('_')[0].replace('task',''))
+                if not subdirs[subnum].has_key(tasknum):
+                    subdirs[subnum][tasknum]=[]
+                subdirs[subnum][tasknum].append(runnum)
+
+    for s in subdirs.iterkeys():
+              for t in subdirs[s].iterkeys():
+                mk_level2_fsf(taskid,s,t,subdirs[s][t],basedir)
+
+if __name__ == '__main__':
+    main()
