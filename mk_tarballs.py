485528a1a13aa5b31d86bcdd84c9a4763832e417
Russell Poldrack <poldrack@login4.ls4.tacc.utexas.edu>
2011-09-04 15:37:19 -0500

    changed mk_tarballs to a single tarball

--- mk_tarballs.py
+++ mk_tarballs.py
@@ -5,7 +5,12 @@ tbdir='/corral/utexas/poldracklab/openfmri/tarballs'
 import sys
 from run_shell_cmd import *
 
-sublist=sys.argv[1]
+#dataset=sys.argv[1]
+dataset='ds107'
+
+sublist='sublist_%s'%dataset
+
+tarcmd='tar zcvf %s/%s.tgz '%(tbdir,dataset)
 
 f=open(sublist,'r')
 for l in f.readlines():
@@ -18,8 +23,8 @@ for l in f.readlines():
     cmd='cp %s/README* %s'%(l_s[0],l)
     print cmd
     run_shell_cmd(cmd)
-    cmd='tar zcvf %s/%s.tgz %s/behav %s/BOLD/*/bold.nii.gz %s/anatomy/highres*.nii.gz %s/anatomy/inplane*.nii.gz %s/README* %s/*key.txt'%(tbdir,subcode,l,l,l,l,l,l)
-    print cmd
-    run_shell_cmd(cmd)
+    tarcmd=tarcmd+'%s/behav %s/BOLD/*/bold.nii.gz %s/anatomy/highres*.nii.gz %s/anatomy/inplane*.nii.gz %s/README* %s/*key.txt '%(l,l,l,l,l,l)
 f.close()
+print cmd
+run_shell_cmd(tarcmd)
 
