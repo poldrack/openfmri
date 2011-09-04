f6b40779f3ff9df5397867b113c8d849f790be6a
Russell Poldrack <poldrack@login4.ls4.tacc.utexas.edu>
2011-09-04 08:24:11 -0500

    added startup niceties

--- get_betaseries.py
+++ get_betaseries.py
@@ -1,4 +1,18 @@
+#!/usr/bin/env python
+"""get_betaseries.py - wrapper for pybetaseries
+
+USAGE: get_betaseries.py <featdir>
+"""
+
 from pybetaseries import *
 import sys
+
+def usage():
+    """Print the docstring and exit with error."""
+    sys.stdout.write(__doc__)
+    sys.exit(2)
+
+if len(sys.argv)<2:
+    usage()
 
 pybetaseries(sys.argv[1])
