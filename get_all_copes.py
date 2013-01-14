4cea417b38e295168f4d9f233a48f1a1d391fd11
Russ Poldrack <poldrack@gmail.com>
2013-01-14 10:22:33 -0600

    cleanup

--- get_all_copes.py
+++
@@ -1,8 +0,0 @@
-f=open('copelist','r')
-l=f.readlines()
-f.close()
-
-for line in l:
-    line_split=line.strip().split('/')
-    new_name=line.strip().replace('/','_').replace('.gfeat','').replace('.feat','').replace('model','').replace('stats','').replace('__zstat1','')
-    print 'cp %s all_copes/%s'%(line.strip(),new_name)
