db8c67e8993136a1c4f68386859e4d69788b49a3
Russ Poldrack <poldrack@gmail.com>
2013-02-14 16:34:17 -0600

    added check_level1.py

---
+++ pipeline/check_level1.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+"""
+check all level1 models
+"""
+
+
+import glob
+import os,sys
+from featdir import Featdir
+
+if len(sys.argv)>1:
+    modelnum=int(sys.argv[1])
+else:
+    modelnum=1
+    
+exclude_warnings=['at least one EV is (close to) a linear combination ']
+featdirs=glob.glob('sub*/model/model%03d/*.feat'%modelnum)
+
+good_data={}
+subs=[]
+for f in featdirs:
+    subcode=f.split('/')[0]
+    if not subcode in subs:
+        subs.append(subcode)
+        print 'processing',subcode
+    #print 'checking %s'%f
+    taskname,runname=os.path.basename(f).replace('.feat','').split('_')
+    tasknum=int(taskname.replace('task',''))
+    runnum=int(runname.replace('run',''))
+    #print f,tasknum,runnum
+    featdir=Featdir(f)
+    featdir.run_all_checks()
+    warnings=[]
+    for w in featdir.warnings:
+        keep_warning=True
+        for e in exclude_warnings:
+            if w.find(e):
+                keep_warning=False
+        if keep_warning:
+            warnings.append(w)
+    if not len(warnings)==0:
+        print 'PROBLEM:'
+        for w in warnings:
+                 print w
+    else:
+        if not good_data.has_key(tasknum):
+            good_data[tasknum]={}
+        if not good_data[tasknum].has_key(runnum):
+            good_data[tasknum][runnum]=0
+        good_data[tasknum][runnum]+=1
+
+print ''
+print len(subs),' subs found (model %d)'%modelnum
+for task in good_data.keys():
+    for run in good_data[task].keys():
+        print 'task%03d_run%03d: %d good featdirs'%(task,run,good_data[task][run])
+#print good_data
