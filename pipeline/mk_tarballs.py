a2db9456193f7000d3009ff23ae26ac879365e68
Russ Poldrack <poldrack@gmail.com>
2013-03-27 16:51:21 -0500

    major reorg and change in checksums

--- pipeline/mk_tarballs.py
+++ pipeline/mk_tarballs.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python
 
 tbdir='/corral-repl/utexas/poldracklab/openfmri/tarballs'
-basedir='/corral-repl/utexas/poldracklab/openfmri/shared2/'
+basedir='/corral-repl/utexas/poldracklab/openfmri/shared2'
 
-import sys,os
+import sys,os,glob
 from run_shell_cmd import *
 
 try:
@@ -15,18 +15,27 @@ except:
 #datasets=['ds105']
 #, 'ds002', 'ds003', 'ds005', 'ds006', 'ds007', 'ds008', 'ds011', 'ds017A', 'ds017B', 'ds018', 'ds051', 'ds052', 'ds101', 'ds102', 'ds105', 'ds107']
 
-tarballs_to_make={'raw':1,'model':0}
+tarballs_to_make={}
+try:
+    tarballs_to_make['raw']=int(sys.argv[2])
+except:
+    tarballs_to_make['raw']=1
+
+try:
+    tarballs_to_make['model']=int(sys.argv[3])
+except:
+    tarballs_to_make['model']=1
 
 for dataset in datasets:
 
-    sublist=[s for s in os.listdir(basedir+dataset) if s.find('sub')==0]
+    sublist=[s for s in os.listdir(os.path.join(basedir,dataset)) if s.find('sub')==0]
 
     if tarballs_to_make['raw']==1:
-        tarcmd='tar zcvf %s/%s_raw.tgz %s/models %s/*.txt '%(tbdir,dataset,dataset,dataset)
+        tarcmd='cd %s; tar zcvf %s/%s_raw.tgz %s/models %s/README* %s/*.txt '%(basedir,tbdir,dataset,dataset,dataset,dataset)
 
         for s in sublist:
-            l=dataset+'/'+s
-            tarcmd=tarcmd+'%s/behav %s/model/model*/onsets %s/BOLD/*/bold.nii.gz %s/anatomy/highres*.nii.gz %s/anatomy/inplane*.nii.gz %s/README* '%(l,l,l,l,l,l)
+            l=os.path.join(dataset,s)
+            tarcmd=tarcmd+'%s/behav %s/model/model*/onsets  %s/BOLD/*/QA %s/BOLD/*/bold.nii.gz %s/anatomy/highres*.nii.gz %s/anatomy/inplane*.nii.gz %s/README* '%(l,l,l,l,l,l,l)
 
         print tarcmd
         run_shell_cmd(tarcmd)
@@ -36,8 +45,35 @@ for dataset in datasets:
 
         for s in sublist:
             l=dataset+'/'+s
-            tarcmd=tarcmd+'%s/model/model* %s/BOLD/*/bold_mcf.par* %s/README* '%(l,l,l)
+            tarcmd=tarcmd+'%s/model/model* %s/README* '%(l,l)
 
         print tarcmd
         run_shell_cmd(tarcmd)
 
+
+    # now make checksum file
+    if tarballs_to_make['raw']==1:
+        checksumfile='%s/%s_raw_checksums.txt'%(tbdir,dataset)
+        f=open(checksumfile,'w')
+        tarcmd='tar ztf %s/%s_raw.tgz'%(tbdir,dataset)
+        flist=run_shell_cmd(tarcmd)
+        for i in flist:
+            cscmd='md5sum %s'%i
+            output=run_shell_cmd(cscmd)
+            #print cscmd,output
+            if len(output)>0:
+                f.write(output[0]+'\n')
+        f.close()
+
+    if tarballs_to_make['model']==1:
+        checksumfile='%s/%s_model_checksums.txt'%(tbdir,dataset)
+        f=open(checksumfile,'w')
+        tarcmd='tar ztf %s/%s_models.tgz'%(tbdir,dataset)
+        flist=run_shell_cmd(tarcmd)
+        for i in flist:
+            cscmd='md5sum %s'%i
+            output=run_shell_cmd(cscmd)
+            #print cscmd,output
+            if len(output)>0:
+                f.write(output[0]+'\n')
+        f.close()
